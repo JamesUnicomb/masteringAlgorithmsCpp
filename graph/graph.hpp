a13a95352d47b8fc00706321e8e7ecdb8d567311
James Unicomb <james.unicomb@advancednavigation.com>
2023-07-27 15:18:50 +1000

    add scrate work for graph

---
+++ graph/graph.hpp
@@ -0,0 +1,42 @@
+#ifndef __GRAPH_HPP__
+#define __GRAPH_HPP__
+
+#include "../linkedlist/linkedlist.hpp"
+#include "../set/set.hpp"
+
+template <typename T>
+class GraphVertex
+{
+public:
+    GraphVertex(T data) : data(new T(data)) {}
+    T getData() { return *data; }
+
+private:
+    T *data;
+};
+
+template <typename T>
+class AdjacencyList
+{
+public:
+    GraphVertex<T> *&getVertex() { return vertex; }
+
+private:
+    GraphVertex<T> *vertex;
+    Set<GraphVertex<T>> adjlist;
+};
+
+template <typename T>
+class Graph
+{
+public:
+    Graph() : vcnt(0), ecnt(0) {}
+
+private:
+    int vcnt;
+    int ecnt;
+
+    Set<AdjacencyList<T>> adjlist;
+};
+
+#endif
