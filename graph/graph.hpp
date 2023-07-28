a0025e20fe5fbc2fc3af4c39fd307b2faf4cd329
James Unicomb <james.unicomb@advancednavigation.com>
2023-07-28 15:52:34 +1000

    scratch work for graph

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -1,6 +1,7 @@
 #ifndef __GRAPH_HPP__
 #define __GRAPH_HPP__
 
+#include <iostream>
 #include "../linkedlist/linkedlist.hpp"
 #include "../set/set.hpp"
 
@@ -10,6 +11,14 @@ class GraphVertex
 public:
     GraphVertex(T data) : data(new T(data)) {}
     T getData() { return *data; }
+    bool operator<(GraphVertex<T> rhs)
+    {
+        return this->getData() < rhs->getData();
+    }
+    ~GraphVertex()
+    {
+        delete data;
+    }
 
 private:
     T *data;
@@ -19,11 +28,30 @@ template <typename T>
 class AdjacencyList
 {
 public:
+    AdjacencyList(T data) : vertex(new GraphVertex<T>(data)), adj(new Set<GraphVertex<T> *>()) {}
     GraphVertex<T> *&getVertex() { return vertex; }
+    Set<GraphVertex<T> *> *&getAdjacency() { return adj; }
+    void addAdjacent(GraphVertex<T> *neighbor)
+    {
+        adj->insert(neighbor);
+    }
+    bool operator<(AdjacencyList<T> rhs)
+    {
+        return this->getVertex()->getData() < rhs.getVertex()->getData();
+    }
+    bool operator>(AdjacencyList<T> rhs)
+    {
+        return this->getVertex()->getData() > rhs.getVertex()->getData();
+    }
+    bool operator==(AdjacencyList<T> rhs)
+    {
+        return this->getVertex()->getData() == rhs.getVertex()->getData();
+    }
+    ~AdjacencyList() {}
 
 private:
     GraphVertex<T> *vertex;
-    Set<GraphVertex<T>> adj;
+    Set<GraphVertex<T> *> *adj;
 };
 
 template <typename T>
@@ -31,6 +59,38 @@ class Graph
 {
 public:
     Graph() : vcnt(0), ecnt(0) {}
+    int getVertexCount() const { return vcnt; }
+    int getEdgeCount() const { return ecnt; }
+    void addVertex(T data)
+    {
+        adjlists.insert(AdjacencyList<T>(data));
+        vcnt += 1;
+    }
+    void addEdge(T fromNode, T toNode)
+    {
+        AdjacencyList<T> *fromAdjList = find(fromNode);
+        fromAdjList->addAdjacent(find(toNode)->getVertex());
+        ecnt += 1;
+    }
+    void display()
+    {
+        typename Set<AdjacencyList<T>>::iterator vert;
+        typename Set<GraphVertex<T> *>::iterator adj;
+
+        for (vert = adjlists.begin(); vert != adjlists.end(); vert++)
+        {
+            for (adj = vert->getData().getAdjacency()->begin(); adj != vert->getData().getAdjacency()->end(); adj++)
+            {
+                std::cout << vert->getData().getVertex()->getData() << "," << adj->getData()->getData() << std::endl;
+            }
+        }
+    }
+    AdjacencyList<T> *find(T data)
+    {
+        return &adjlists.find(AdjacencyList<T>(data))->getData();
+    }
+
+    ~Graph() {}
 
 private:
     int vcnt;
--- graph/main.cpp
+++ graph/main.cpp
@@ -5,5 +5,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    Graph<int> graph;
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+    graph.addVertex(5);
+
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(3, 2);
+    graph.addEdge(3, 4);
+    graph.addEdge(5, 1);
+
+    cout << "edge list: " << endl;
+    graph.display();
+
+    GraphVertex<int> *g = graph.find(3)->getVertex();
+
+    cout << "find(3) : " << g->getData() << " at address (" << g << ")" << endl;
+    cout << "vertex count = " << graph.getVertexCount() << endl;
+    cout << "edge count = " << graph.getEdgeCount() << endl;
 }
